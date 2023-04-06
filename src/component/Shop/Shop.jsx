2a4d69281aef4d6f59de608824753c7f0f61106d
unknown <emonhossen1000@gmail.com>
2023-04-07 00:12:03 +0600

    btn xart added

--- src/component/Shop/Shop.jsx
+++ src/component/Shop/Shop.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import "./shop.css"
+import Product from '../product/Product';
 const Shop = () => {
     const [products,setProducts]=useState([]);
     useEffect(()=>{
@@ -11,7 +12,12 @@ const Shop = () => {
     return (
         <div className='shop-container'>
             <div className="product-container">
-                    <h2>Products coming here  : {products.length}</h2>
+                {
+                    products.map(product=><Product
+                    key={product.id}
+                    product={product}
+                    ></Product>)
+                }
             </div>
             <div className="cart-container">
                 <h4>Order summary</h4>
